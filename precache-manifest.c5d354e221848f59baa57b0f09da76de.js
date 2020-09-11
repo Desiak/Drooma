9d3d76808d4be69ca2e48fcf131355500442a1fe
Wojtek Wojtunik <desqa16@gmail.com>
2020-09-12 00:02:53 +0200

    Updates

---
+++ precache-manifest.c5d354e221848f59baa57b0f09da76de.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dffa836b4cadf8a4effc9777c918550",
+    "url": "/Drooma/index.html"
+  },
+  {
+    "revision": "869f6cc043f94f712e6c",
+    "url": "/Drooma/static/css/main.27d66416.chunk.css"
+  },
+  {
+    "revision": "7ee602ef19f556c9dcd0",
+    "url": "/Drooma/static/js/2.6790c209.chunk.js"
+  },
+  {
+    "revision": "d2907550b0749ad10542a424f45133ae",
+    "url": "/Drooma/static/js/2.6790c209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869f6cc043f94f712e6c",
+    "url": "/Drooma/static/js/main.67c7b2f0.chunk.js"
+  },
+  {
+    "revision": "80950f70b0f2f58fbe18",
+    "url": "/Drooma/static/js/runtime-main.4bae86af.js"
+  },
+  {
+    "revision": "052f120a81f9bc4ac822249914f27c70",
+    "url": "/Drooma/static/media/crash.052f120a.svg"
+  },
+  {
+    "revision": "ab5a85d3d5510ba0474a94d80c23af20",
+    "url": "/Drooma/static/media/crash.ab5a85d3.wav"
+  },
+  {
+    "revision": "b6aa41b90095d508de1b43c08928d118",
+    "url": "/Drooma/static/media/crash.b6aa41b9.wav"
+  },
+  {
+    "revision": "cd5ad46abfa2d8086efe0ff2662563c7",
+    "url": "/Drooma/static/media/crash.cd5ad46a.wav"
+  },
+  {
+    "revision": "deef365dd77b50ee0698b433cbcb7334",
+    "url": "/Drooma/static/media/crash.deef365d.wav"
+  },
+  {
+    "revision": "eb2327f93d4141c3e93362363a3d35cb",
+    "url": "/Drooma/static/media/crash.eb2327f9.wav"
+  },
+  {
+    "revision": "12c09b0b328c006bdc16b4dfe4b00326",
+    "url": "/Drooma/static/media/crash1.12c09b0b.wav"
+  },
+  {
+    "revision": "0238b6e2e49c0df50dad51516b478541",
+    "url": "/Drooma/static/media/crash3.0238b6e2.wav"
+  },
+  {
+    "revision": "4e4dfd6fb2b8450262178ca1457d653f",
+    "url": "/Drooma/static/media/floor-tom.4e4dfd6f.wav"
+  },
+  {
+    "revision": "790ea490e3e31e60f442a24ae675ba26",
+    "url": "/Drooma/static/media/floor-tom.790ea490.wav"
+  },
+  {
+    "revision": "c3b55e8f53b4ddca2e0da284e08ecd46",
+    "url": "/Drooma/static/media/floor-tom.c3b55e8f.wav"
+  },
+  {
+    "revision": "cd69ed72217e60a8593fe7d10bdcd7db",
+    "url": "/Drooma/static/media/floor-tom.cd69ed72.wav"
+  },
+  {
+    "revision": "ec48b3bc90cc6cb3010db6c205e5aea7",
+    "url": "/Drooma/static/media/floor-tom.ec48b3bc.wav"
+  },
+  {
+    "revision": "f504e2c7938ce425706d2dae22f5f9d5",
+    "url": "/Drooma/static/media/floor.f504e2c7.svg"
+  },
+  {
+    "revision": "075aaf260e691dc7129cab324781b4f5",
+    "url": "/Drooma/static/media/hihat.075aaf26.wav"
+  },
+  {
+    "revision": "1b898f2e9b842837b9c3b4851a649123",
+    "url": "/Drooma/static/media/hihat.1b898f2e.wav"
+  },
+  {
+    "revision": "3d75913febf4d5fc5bf6a2fe0cde516a",
+    "url": "/Drooma/static/media/hihat.3d75913f.svg"
+  },
+  {
+    "revision": "721ed157da12f3ec8c5ef654f7170a9e",
+    "url": "/Drooma/static/media/hihat.721ed157.wav"
+  },
+  {
+    "revision": "a40496fc3c0b1359c4ab937d79bbed94",
+    "url": "/Drooma/static/media/hihat.a40496fc.wav"
+  },
+  {
+    "revision": "db35e09703f7eecc43b96b9b54eee7e4",
+    "url": "/Drooma/static/media/hihat.db35e097.wav"
+  },
+  {
+    "revision": "0dbeed614a2e377768c35a544392b494",
+    "url": "/Drooma/static/media/kick.0dbeed61.wav"
+  },
+  {
+    "revision": "2f633cd4f90316d084da85b5556f072d",
+    "url": "/Drooma/static/media/kick.2f633cd4.wav"
+  },
+  {
+    "revision": "302c7570f355865d9d5a73afc9c80dbd",
+    "url": "/Drooma/static/media/kick.302c7570.svg"
+  },
+  {
+    "revision": "7d13e60b01ce036f1d45ba08be190a2f",
+    "url": "/Drooma/static/media/kick.7d13e60b.wav"
+  },
+  {
+    "revision": "d929c19d06854ca3f8a16873f496dfbc",
+    "url": "/Drooma/static/media/kick.d929c19d.wav"
+  },
+  {
+    "revision": "ef418f7b8b8f13c8bbd9a07172042704",
+    "url": "/Drooma/static/media/kick.ef418f7b.wav"
+  },
+  {
+    "revision": "3afcfae3605bc1b0092e0bd26f8b5f55",
+    "url": "/Drooma/static/media/ride.3afcfae3.svg"
+  },
+  {
+    "revision": "6cf48bfc37a22f64b8a2885f4d9d264b",
+    "url": "/Drooma/static/media/ride.6cf48bfc.wav"
+  },
+  {
+    "revision": "6e8ec3c37e30032c18555d9895eae0bd",
+    "url": "/Drooma/static/media/ride.6e8ec3c3.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/Drooma/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "cfe55a1141b65bb679be74a2f9a77d8e",
+    "url": "/Drooma/static/media/ride.cfe55a11.wav"
+  },
+  {
+    "revision": "d2a2ba185c3e43a348a6ebe86e18256d",
+    "url": "/Drooma/static/media/ride.d2a2ba18.wav"
+  },
+  {
+    "revision": "42b19bca5871d8ff585e8bac19f99a53",
+    "url": "/Drooma/static/media/ride1.42b19bca.wav"
+  },
+  {
+    "revision": "29a010d1c63059688ef2599ddc6b02cd",
+    "url": "/Drooma/static/media/snare.29a010d1.wav"
+  },
+  {
+    "revision": "34d51e6acbdcabf68f9bb3698fe6b861",
+    "url": "/Drooma/static/media/snare.34d51e6a.wav"
+  },
+  {
+    "revision": "4f33dc854e958444dc2624690ebc41c9",
+    "url": "/Drooma/static/media/snare.4f33dc85.wav"
+  },
+  {
+    "revision": "5355dc99df42fe96295dfc1098d001da",
+    "url": "/Drooma/static/media/snare.5355dc99.wav"
+  },
+  {
+    "revision": "7b74c0328dbc4fdf383e09d957d878fe",
+    "url": "/Drooma/static/media/snare.7b74c032.svg"
+  },
+  {
+    "revision": "be9ea3c5fa665db6f17d50c75ee9b365",
+    "url": "/Drooma/static/media/snare.be9ea3c5.wav"
+  },
+  {
+    "revision": "4b4a672acb920f4aaeef9dfe9568ee43",
+    "url": "/Drooma/static/media/tom1.4b4a672a.svg"
+  },
+  {
+    "revision": "a44e459442b881a07ad56f6fc713a597",
+    "url": "/Drooma/static/media/tom1.a44e4594.wav"
+  },
+  {
+    "revision": "c624c4caeda0bfb48aee783e9e4bbc5d",
+    "url": "/Drooma/static/media/tom1.c624c4ca.wav"
+  },
+  {
+    "revision": "d8d2c279f5212c67ad4b606a11f82a80",
+    "url": "/Drooma/static/media/tom1.d8d2c279.wav"
+  },
+  {
+    "revision": "db9589addbf2e89b204af8e21edc5546",
+    "url": "/Drooma/static/media/tom1.db9589ad.wav"
+  },
+  {
+    "revision": "e34ebdd21920905ab790c303914e5f54",
+    "url": "/Drooma/static/media/tom1.e34ebdd2.wav"
+  },
+  {
+    "revision": "75057526e15d86a7fe31f25dc0ff5887",
+    "url": "/Drooma/static/media/tom2.75057526.wav"
+  },
+  {
+    "revision": "781fe487b44280735fa90f91aa8d946e",
+    "url": "/Drooma/static/media/tom2.781fe487.wav"
+  },
+  {
+    "revision": "7a0b1b528cd9b8951535cee4a04f8ca4",
+    "url": "/Drooma/static/media/tom2.7a0b1b52.wav"
+  },
+  {
+    "revision": "941cce27bcbadbabd722b7d466c18666",
+    "url": "/Drooma/static/media/tom2.941cce27.svg"
+  },
+  {
+    "revision": "99cf901000ff47a2c7b3d331e6b23085",
+    "url": "/Drooma/static/media/tom2.99cf9010.wav"
+  },
+  {
+    "revision": "b0f09c311b67735c7dd3b7def8a62bc4",
+    "url": "/Drooma/static/media/tom2.b0f09c31.wav"
+  }
+]);
